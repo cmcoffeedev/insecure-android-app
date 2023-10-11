414624e7f054e54594a2e799103207b769f9ccc9
Christopher Coffee <chrisc1891@gmail.com>
2023-10-10 22:14:48 -0700

    Login UI

--- app/src/main/java/com/cmcoffeedev/insecureandroidapp/MainActivity.kt
+++ app/src/main/java/com/cmcoffeedev/insecureandroidapp/MainActivity.kt
@@ -1,15 +1,32 @@
 package com.cmcoffeedev.insecureandroidapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.cmcoffeedev.insecureandroidapp.ui.theme.InsecureAndroidAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +39,64 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ScreenLogin()
                 }
             }
         }
     }
 }
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ScreenLogin() {
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    val context = LocalContext.current
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.padding(20.dp),
+    ) {
+        TextField(
+            value = username,
+            onValueChange = { username = it },
+            label = { Text(stringResource(R.string.username)) },
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        )
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text(stringResource(R.string.password)) },
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        )
+
+        Button(
+            onClick = {
+                //show toast
+                Toast.makeText(
+                    context,
+                    "Logging in",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        ) {
+            Text("Login")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     InsecureAndroidAppTheme {
-        Greeting("Android")
+        ScreenLogin()
     }
 }
